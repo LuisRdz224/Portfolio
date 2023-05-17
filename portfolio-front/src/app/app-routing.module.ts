f13780828547872671687a9afd849a7e24948c39
LuisRdz224 <luisrdzh224@gmail.com>
2023-05-17 09:55:37 -0600

    App creation

---
+++ portfolio-front/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/components/landing/landing.component';
+
+const routes: Routes = [
+  { path: 'home', component: LandingComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
